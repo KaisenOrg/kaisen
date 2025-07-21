64ed79080f89778da28f472c274cf58ba6f667f8
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-07-20 20:41:18 -0400

    refactor: reorganizar importações e simplificar o componente Providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -9,27 +9,22 @@ import { GlobalPopover } from '@/components/ui/global-popover';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {process.env.NEXT_PUBLIC_DFX_NETWORK === 'ic' ? (
-        <IdentityKitProvider
-          authType={IdentityKitAuthType.DELEGATION}
-          signerClientOptions={{
-            targets: [
-              process.env.NEXT_PUBLIC_CANISTER_ID_KAI_BACKEND!,
-              process.env.NEXT_PUBLIC_CANISTER_ID_TRACKS_BACKEND!,
-              process.env.NEXT_PUBLIC_CANISTER_ID_CHATS_BACKEND!,
-              process.env.NEXT_PUBLIC_CANISTER_ID_NFT_CERTIFICATES!,
-              process.env.NEXT_PUBLIC_CANISTER_ID_INTERNET_IDENTITY!,
-            ]
-          }}>
-          {children}
-          <GlobalPopover />
-        </IdentityKitProvider>
-      ) : (
+      <IdentityKitProvider
+        authType={IdentityKitAuthType.DELEGATION}
+        signerClientOptions={{
+          targets: [
+            process.env.NEXT_PUBLIC_CANISTER_ID_KAI_BACKEND!,
+            process.env.NEXT_PUBLIC_CANISTER_ID_TRACKS_BACKEND!,
+            process.env.NEXT_PUBLIC_CANISTER_ID_CHATS_BACKEND!,
+            process.env.NEXT_PUBLIC_CANISTER_ID_NFT_CERTIFICATES!,
+            process.env.NEXT_PUBLIC_CANISTER_ID_INTERNET_IDENTITY!,
+          ]
+        }}>
         <DevAuthProvider>
           {children}
           <GlobalPopover />
         </DevAuthProvider>
-      )}
+      </IdentityKitProvider>
     </ThemeProvider>
   );
 }
